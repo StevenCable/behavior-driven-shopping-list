b95057b22c7c794bec19f4e751476410723dc6ef
Joshua Lee <joshualee@iconfigure.org>
2016-12-10 20:50:57 -1000

    updated code for tests for shopping list

---
+++ js/shopping_list_item.js
@@ -0,0 +1,18 @@
+class ShoppingListItem {
+  constructor(name, description){
+    this.name = name;
+    this.description = description;
+    this.is_done = false;
+
+  }
+  check(){
+    this.is_done = false;
+
+  }
+
+  uncheck(){
+    this.is_done = false;
+  }
+}
+
+
