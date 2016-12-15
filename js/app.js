5a9290d68ddef529d15afd89bfa0894f04e24b09
Joshua Lee <joshualee@iconfigure.org>
2016-12-14 21:12:32 -1000

    updated index file, removed form element

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//Index html Connection
+let loadButton =document.getElementById('add_shopping_list_item_button');
+
+loadButton.addEventListener('click', add_to_shopping_list);
+
+function add_to_shopping_list() {
+
+}
